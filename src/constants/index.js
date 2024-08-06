2d4a180b3ed64b309f7a9a3262df2b87b6611716
Tejas <tejasabuday@gmail.com>
2024-08-07 00:09:50 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,338 @@
+import {
+  benefitIcon1,
+  benefitIcon2,
+  benefitIcon3,
+  benefitIcon4,
+  benefitImage2,
+  benefitImage3,
+  benefitImage4,
+  benefitImage5,
+  benefitImage6,
+  benefitImage7,
+  chromecast,
+  disc02,
+  discord,
+  discordBlack,
+  facebook,
+  figma,
+  file02,
+  framer,
+  homeSmile,
+  instagram,
+  notification2,
+  notification3,
+  notification4,
+  notion,
+  photoshop,
+  plusSquare,
+  protopie,
+  raindrop,
+  recording01,
+  recording03,
+  roadmap1,
+  roadmap2,
+  roadmap3,
+  roadmap4,
+  searchMd,
+  slack,
+  sliders04,
+  telegram,
+  twitter,
+  yourlogo,
+} from "../assets";
+
+export const navigation = [
+  {
+    id:"9",
+    title: "Home",
+    url: "#hero",
+  },
+  {
+    id: "0",
+    title: "Services",
+    url: "#features",
+  },
+    {
+    id: "1",
+    title: "Why Us ?",
+    url: "#how-to-use",
+  },
+  {
+    id: "2",
+    title: "Team ",
+    url: "#pricing",
+  },
+ 
+  {
+    id: "3",
+    title: "Roadmap",
+    url: "#roadmap",
+  },
+  
+];
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+export const notificationImages = [notification4, notification3, notification2];
+
+export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+
+export const brainwaveServices = [
+  "Explore venues",
+  "Check availability",
+  "Advertise your events",
+];
+
+export const brainwaveServicesIcons = [
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+];
+
+export const roadmap = [
+  
+  {
+    id: "0",
+    title: "Get the Right and Best facilities on board.",
+    text: "We plan on giving our customers the best of the best and plan to get the best vendors on board with us to list their facalities on our site and app.",
+    date: "September 2024",
+    status: "progress",
+    imageUrl: roadmap2,
+  },
+  {
+    id: "1",
+    title: "Develop a user friendly website",
+    text: "We have started Developing a very user friendly website that makes it easy for Teens and adults to use and plan.",
+    date: "September 2024",
+    status: "ongoing",
+    imageUrl: roadmap1,
+    colorful: true,
+  },
+  {
+    id: "2",
+    title: "Develop a user friendly Application",
+    text: "We plan on developing a user friendly application for people to use making it even easier and accesible for them to book and plan their parties",
+    date: "December 2024",
+    status: "progress",
+    imageUrl: roadmap3,
+    colorful: true,
+  },
+  {
+    id: "3",
+    title: "Launch our Services and be the best out there",
+    text: "We plan to put in all our hardwork to be the best in the buisness and help out as many people as we can.",
+    date: "September 2024",
+    status: "progress",
+    imageUrl: roadmap4,
+  },
+];
+
+export const collabText =
+  "We help you host every event of yours may it be your first music show or your first standup you wanna sell tickets and let people know you exist we are here for you no matter how big or how small we celebrate with you.";
+
+  export const collabContent = [
+    {
+      id: "0",
+      title: "Effortless Event Hosting",
+      text: "Easily host any event, whether it's your birthday or your first show, with our seamless tools.",
+    },
+    {
+      id: "1",
+      title: "Connect with Like-Minded People",
+      text: "Sell tickets, build a community, and attend parties together—all with just a single click.",
+    },
+    {
+      id: "2",
+      title: "Discover Top Venues",
+      text: "Find the best locations to host your event or attend others' events effortlessly.",
+    },
+  ];
+
+export const collabApps = [
+  {
+    id: "0",
+    title: "figma",
+    icon: figma,
+    width: 34,
+    height: 36,
+    link: "mailto:pyop.india@gmail.com",
+  },
+  {
+    id: "1",
+    title: "notion",
+    icon: notion,
+    width: 34,
+    height: 36,
+    link: "https://x.com/pyop_in",
+  },
+  {
+    id: "2",
+    title: "Discord",
+    icon: discord,
+    width: 36,
+    height: 28,
+    link: "https://www.facebook.com/profile.php?id=61562588716322",
+  },
+  {
+    id: "3",
+    title: "Slack",
+    icon: slack,
+    width: 38,
+    height: 50,
+    link: "https://www.instagram.com/pyop.in",
+  },
+  {
+    id: "4",
+    title: "Photoshop",
+    icon: photoshop,
+    width: 34,
+    height: 34,
+    link: "https://www.adobe.com/products/photoshop.html",
+  },
+  {
+    id: "5",
+    title: "Protopie",
+    icon: protopie,
+    width: 34,
+    height: 34,
+    link: "https://www.protopie.io",
+  },
+  {
+    id: "6",
+    title: "Framer",
+    icon: framer,
+    width: 26,
+    height: 34,
+    link: "https://www.framer.com",
+  },
+  {
+    id: "7",
+    title: "Raindrop",
+    icon: raindrop,
+    width: 38,
+    height: 32,
+    link: "https://raindrop.io",
+  },
+];
+
+
+export const pricing = [
+  
+  {
+    id: "1",
+    title: "Tejas Abhuday Pandey",
+    description: "Co-Founder",
+    
+    email:"tejasabhuday@gmail.com",
+    features: [
+      "Ever felt like hosting or planning a party was difficult we are here to fix that it's time to get started.",
+      "A budding and hardworking Entrepreneur.",
+      "Would give it all that I got to achieve my goals.",
+    ],
+  },
+  {
+    id: "0",
+    title: "Aman Singh",
+    description: "Co-Founder",
+   
+    email:"1803singh.aman@gmail.com",
+    features: [
+      "Ever felt like selling tickets from person to person is a lot of work well not anymore.",
+      "Transforming ideas into reality with strategic vision and dedication",
+      "Building a business foundation on persistence and determination",
+    ],
+  },
+  {
+    id: "2",
+    title: "Aryan Arvind Singh",
+    description: "Co-Founder",
+          
+    
+    email:"aryan010420@gmail.com",
+    features: [
+      "We are dedicated and focused to make a change.",
+      "We wanna make it easy and accesible for everyone to celebrate even their smallest achievements.",
+      "We will work hard till the time we aren't the best.",
+    ],
+  },
+];
+
+
+
+export const benefits = [
+  {
+    id: "0",
+    title: "Host your own events",
+    text: "Pyop empowers users to effortlessly organize and host diverse events such as music shows, book readings, and open mics, facilitating seamless event management from planning to execution and selling you very own tickets.",
+    backgroundUrl: "./src/assets/benefits/card-1.svg",
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "1",
+    title: "Discover Artists",
+    text: "Pyop simplifies discovering emerging and established artists across diverse creative fields, making it easy to find new talents and explore innovative voices in music, visual arts, and more.",
+    backgroundUrl: "./src/assets/benefits/card-2.svg",
+    imageUrl: benefitImage3,
+    light: true,
+  },
+  {
+    id: "2",
+    title: "Plan your parties",
+    text: "With Pyop, effortlessly plan your perfect party, streamlining every detail from guest lists to decorations, ensuring a memorable event without the stress.",
+    backgroundUrl: "./src/assets/benefits/card-3.svg",
+    imageUrl: benefitImage4,
+  },
+  {
+    id: "3",
+    title: "Find Vendors",
+    text: "Pyop simplifies finding party vendors by connecting users with top-notch services and suppliers for their events, ensuring effortless and efficient planning for any occasion.",
+    backgroundUrl: "./src/assets/benefits/card-4.svg",
+    imageUrl: benefitImage5,
+    light: true,
+  },
+  {
+    id: "4",
+    title: "Explore & Plan HouseParties",
+    text: "Pyop simplifies the process of exploring and planning house parties, ensuring every gathering is perfectly organised and memorable.",
+    backgroundUrl: "./src/assets/benefits/card-5.svg",
+    imageUrl: benefitImage6,
+  },
+  {
+    id: "5",
+    title: "Explore Nightlife",
+    text: "Pyop makes discovering nearby nightlife effortless, guiding you to vibrant venues and events with ease.",
+    backgroundUrl: "./src/assets/benefits/card-6.svg",
+    imageUrl: benefitImage7,
+    light: true,
+  },
+];
+
+export const socials = [
+  
+  {
+    id: "1",
+    title: "twitter",
+    iconUrl: twitter,
+    url: "https://x.com/pyop_in",
+  },
+  {
+    id: "2",
+    title: "Instagram",
+    iconUrl: instagram,
+    url: "https://www.instagram.com/pyop.in",
+  },
+  {
+    id: "3",
+    title: "Telegram",
+    iconUrl: figma,
+    url: "mailto:pyop.india@gmail.com",
+  },
+  {
+    id: "4",
+    title: "Facebook",
+    iconUrl: facebook,
+    url: "https://www.facebook.com/profile.php?id=61562588716322",
+  },
+];
